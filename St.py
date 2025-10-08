474910c9eff276b2482219af9b77992cf200804b
zaraza1005 <125798334+zaraza1005@users.noreply.github.com>
2025-10-08 15:05:32 +0300

    Update St.py

--- St.py
+++ St.py
@@ -1,3 +1,7 @@
 import streamlit as st
 st.title("Hello Streamlit")
 st.write("Hello world")
+
+name = st. text_input (" Введiть ваше iм ’я:")
+2 if st. button (" Привiтати "):
+3 st. success (f"Привiт , { name }!")
